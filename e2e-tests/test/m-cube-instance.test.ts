e873e6785117de0235ea32d6380957821911c847
Danilo Arantes <dfilho@purdue.edu>
2024-05-27 18:48:32 -0300

    Added e2e tests for instanced cubes

--- e2e-tests/test/m-cube-instance.test.ts
+++ e2e-tests/test/m-cube-instance.test.ts
@@ -1,36 +1,83 @@
-describe("m-cube", () => {
-  test("instanced attribute", async () => {
+import { takeAndCompareScreenshot } from "./testing-utils";
+
+const labelCoords = {
+  addCubes: {
+    x: 150,
+    y: 110,
+  },
+  removeCubes: {
+    x: 325,
+    y: 110,
+  },
+  minus: {
+    x: 130,
+    y: 215,
+  },
+  plus: {
+    x: 365,
+    y: 215,
+  },
+};
+
+const cubeCoords = [
+  { x: 663, y: 885 },
+  { x: 585, y: 885 },
+  { x: 365, y: 885 },
+  { x: 505, y: 885 },
+  { x: 220, y: 885 },
+];
+
+describe("instancing", () => {
+  test("instanced mesh visible and clickable", async () => {
     const page = await __BROWSER_GLOBAL__.newPage();
 
     await page.setViewport({ width: 1024, height: 1024 });
+    await page.goto("http://localhost:7079/m-cube-instanced-test.html/reset");
 
-    await page.goto("http://localhost:7079/m-cube-instance-test.html/reset");
+    await page.waitForSelector("#addCubes");
+    await page.click("canvas", { offset: labelCoords.addCubes });
 
-    await page.waitForSelector("m-cube[color='red']");
+    await Promise.all(
+      cubeCoords.map(async (coord) => await page.click("canvas", { offset: coord })),
+    );
 
-    expect(
-      await page.evaluate(() => {
-        const cube = (document.querySelector("m-cube") as any).getScene();
-        return cube.renderer.info.render.calls as number;
-      }),
-    ).toBe(6);
+    await page.waitForSelector("[data-instanced='5']");
+    await takeAndCompareScreenshot(page);
 
-    // Switch to instanced
-    {
-      await page.evaluate(() => {
-        const cubes = document.querySelectorAll("m-cube");
-        for (const cube of cubes) {
-          cube.setAttribute("instanced", "true");
-        }
-      });
+    await page.close();
+  }, 60000);
+
+  test("instanced mesh can be removed in any order", async () => {
+    const page = await __BROWSER_GLOBAL__.newPage();
+
+    await page.setViewport({ width: 1024, height: 1024 });
+    await page.goto("http://localhost:7079/m-cube-instanced-test.html/reset");
+
+    // Add 10 cubes
+    await page.waitForSelector("#addCubes");
+    await page.click("canvas", { offset: labelCoords.addCubes });
+
+    await Promise.all(
+      cubeCoords.map(async (coord) => await page.click("canvas", { offset: coord })),
+    );
+
+    await page.waitForSelector("[data-instanced='5']");
+    await takeAndCompareScreenshot(page);
+
+    // Set value to 1
+    await Promise.all(
+      Array.from({ length: 9 }).map(
+        async () => await page.click("canvas", { offset: labelCoords.minus }),
+      ),
+    );
+    await page.waitForSelector("[data-value='1']");
+
+    // Unregister some cubes such that the indexes need to be shifted
+    for (let i = 0; i < 8; i++) {
+      await page.click("canvas", { offset: labelCoords.removeCubes });
+      await page.waitForSelector(`[data-cube-count='${9 - i}']`);
+      await takeAndCompareScreenshot(page);
     }
-
-    expect(
-      await page.evaluate(() => {
-        const cube = (document.querySelector("m-cube") as any).getScene();
-        return cube.renderer.info.render.calls as number;
-      }),
-    ).toBe(1);
 
     await page.close();
   }, 60000);
